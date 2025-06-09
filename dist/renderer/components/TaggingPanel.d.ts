b0372eb2a75aac00a3dc1f966be734aff58b5538
kauredo <vaskafig@gmail.com>
2025-06-09 15:05:17 +0100

    WIP reset

--- dist/renderer/components/TaggingPanel.d.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-interface TaggingPanelProps {
-    currentTime: number;
-    onAddTag: (tagType: string, description?: string, player?: string) => void;
-    isVideoLoaded: boolean;
-}
-export declare const TaggingPanel: React.FC<TaggingPanelProps>;
-export {};
