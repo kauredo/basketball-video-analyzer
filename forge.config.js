10dac9e526fe77c6eb4c15bf2caaa578774c199c
kauredo <vaskafig@gmail.com>
2025-06-26 01:22:04 +0100

    second version

--- forge.config.js
+++ forge.config.js
@@ -5,6 +5,7 @@ module.exports = {
     asar: true,
     name: "Basketball Video Analyzer",
     executableName: "basketball-clip-cutter",
+    appBundleId: "com.yourname.basketball-clip-cutter",
     icon: "./assets/icon",
     osxSign: false,
     osxNotarize: false,
@@ -16,8 +17,20 @@ module.exports = {
       platforms: ["darwin"],
     },
     {
+      name: "@electron-forge/maker-dmg",
+      platforms: ["darwin"],
+      config: {
+        name: "Basketball Video Analyzer",
+        format: "UDZO",
+      },
+    },
+    {
       name: "@electron-forge/maker-squirrel",
       platforms: ["win32"],
+      config: {
+        name: "basketball_clip_cutter",
+        setupExe: "BasketballVideoAnalyzerSetup.exe",
+      },
     },
   ],
   plugins: [
