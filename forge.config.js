f638e025d336ee5b22d10cee66dfb0251418b527
kauredo <vaskafig@gmail.com>
2025-06-27 13:58:04 +0100

    windows fix 8

--- forge.config.js
+++ forge.config.js
@@ -1,4 +1,5 @@
-// const { FuseV1Options, FuseVersion } = require("@electron/fuses");
+const { FusesPlugin } = require("@electron-forge/plugin-fuses");
+const { FuseV1Options, FuseVersion } = require("@electron/fuses");
 
 module.exports = {
   packagerConfig: {
@@ -21,29 +22,27 @@ module.exports = {
   rebuildConfig: {},
   makers: [
     {
+      name: "@electron-forge/maker-squirrel",
+      config: {},
+    },
+    {
       name: "@electron-forge/maker-zip",
       platforms: ["darwin"],
     },
     {
-      name: "@electron-forge/maker-dmg",
-      platforms: ["darwin"],
+      name: "@electron-forge/maker-deb",
       config: {
-        name: "Basketball Video Analyzer",
-        format: "UDZO",
+        name: "basketball-clip-cutter",
+        description: "Basketball Video Analyzer",
+        category: "Video",
       },
     },
     {
-      name: "@electron-forge/maker-squirrel",
-      platforms: ["win32"],
+      name: "@electron-forge/maker-rpm",
       config: {
-        name: "basketball_clip_cutter",
-        setupExe: "BasketballVideoAnalyzerSetup.exe",
-        setupIcon: "./assets/icon.ico",
-        loadingGif: "./assets/loading.gif",
-        noMsi: true,
-        remoteReleases: "",
-        certificateFile: process.env.WINDOWS_CERTIFICATE_FILE,
-        certificatePassword: process.env.WINDOWS_CERTIFICATE_PASSWORD,
+        name: "basketball-clip-cutter",
+        description: "Basketball Video Analyzer",
+        category: "Video",
       },
     },
   ],
@@ -52,5 +51,16 @@ module.exports = {
       name: "@electron-forge/plugin-auto-unpack-natives",
       config: {},
     },
+    // Fuses are used to enable/disable various Electron functionality
+    // at package time, before code signing the application
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.EnableCookieEncryption]: true,
+      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+      [FuseV1Options.EnableNodeCliInspectArguments]: false,
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      [FuseV1Options.OnlyLoadAppFromAsar]: true,
+    }),
   ],
 };
